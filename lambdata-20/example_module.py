cf51f8cb3f658b4c3de204d865287a81021be5c5
nwdelafu <nick.delafuente1@gmail.com>
2020-10-26 12:16:33 -0600

    Unit3Sprint1Module1 Code Along

---
+++ lambdata-20/example_module.py
@@ -0,0 +1,17 @@
+"""Lambdata - a collection of Data Science helper functions""" 
+
+# accessing libraries through pipenv
+import pandas as pd 
+import numpy as np 
+
+
+FAVORITE_NUMBERS = [2.71, 101, 55, 12, 3.14]
+
+def df_cleaner(df):
+  """Cleans a DF"""
+  # TODO - implement df_cleaner
+  pass
+
+def df_destoryer(df):
+  """Destroys a DF"""
+  pass
